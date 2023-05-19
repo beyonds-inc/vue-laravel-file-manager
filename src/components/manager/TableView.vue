db65618f5dc2bda5588c4910b3f212ccf3cb7895
Gyuha Choi <choi@beyonds-inc.com>
2023-05-19 21:48:50 +0900

    New表示、説明全文表示機能の実装

--- src/components/manager/TableView.vue
+++ src/components/manager/TableView.vue
@@ -10,12 +10,8 @@
                             <i class="bi bi-sort-up" v-show="sortSettings.direction === 'up'" />
                         </template>
                     </th>
-                    <th class="w-35" v-on:click="sortBy('description')">
+                    <th class="w-35">
                         {{ lang.manager.table.description }}
-                        <template v-if="sortSettings.field === 'description'">
-                            <i class="bi bi-sort-down" v-show="sortSettings.direction === 'down'" />
-                            <i class="bi bi-sort-up" v-show="sortSettings.direction === 'up'" />
-                        </template>
                     </th>
                     <th class="w-10" v-on:click="sortBy('size')">
                         {{ lang.manager.table.size }}
@@ -74,12 +70,22 @@
                     v-on:click="selectItem('files', file.path, $event)"
                     v-on:dblclick="selectAction(file.path, file.extension)"
                     v-on:contextmenu.prevent="contextMenu(file, $event)"
+                    style="position: relative;"
                 >
                     <td class="fm-content-item unselectable" v-bind:class="acl && file.acl === 0 ? 'text-hidden' : ''">
                         <i class="bi" v-bind:class="extensionToIcon(file.extension)" />
-                        {{ file.filename ? file.filename : file.basename }}
+                        {{ file.filename ? abbriviatedString(file.filename, 15) : abbriviatedString(file.basename, 15) }}
+                        <span v-if="isFileNew(file.timestamp)" class="new-indicator">NEW</span>
                     </td>
-                    <td>{{ file.description }}</td>
+                    <td class="description" @mouseenter="showFullDescription(index)" @mouseleave="hideFullDescription()">
+                        <p>{{ abbriviatedString(file.description, 20) }}</p>
+                        <div class="description-modal-wrapper">
+                            <div v-if="!hasClosed && showFlag && showIndex === index && file.description.length > 0" class="description-modal" :style="{ marginTop: '-' + windowTop + 'px' }">
+                                <button type="button" class="btn-close" aria-label="Close" @click="closeFullDescription()" />
+                                <p class="description-modal-text">{{ file.description }}</p>
+                            </div>
+                        </div>
+                    </td>
                     <td>{{ bytesToHuman(file.size) }}</td>
                     <td>
                         {{ file.extension }}
@@ -104,6 +110,14 @@ export default {
     props: {
         manager: { type: String, required: true },
     },
+    data() {
+        return {
+            showFlag: false,
+            hasClosed: false,
+            showIndex: null,
+            windowTop: null,
+        };
+    },
     computed: {
         /**
          * Sort settings
@@ -121,6 +135,66 @@ export default {
         sortBy(field) {
             this.$store.dispatch(`fm/${this.manager}/sortBy`, { field, direction: null });
         },
+
+        /**
+         * ファイル説明が長い場合、省略形で表示する。
+         * @param {string} string
+         * @param {number} str_length
+         * @returns {string}
+         */
+        abbriviatedString(string, str_length) {
+            if (!string) return string;
+            return string.length > str_length ? string.substring(0, str_length) + "..." : string;
+        },
+
+        /**
+         * ファイル説明全文を表示する。
+         * @param {number} index
+         */
+        showFullDescription(index) {
+            this.showIndex = index;
+            setTimeout(() => {
+                if (this.showIndex === index) {
+                    this.showFlag = true;
+                }
+            }, 1000);
+        },
+
+        /**
+         * ファイル説明を隠す。
+         * 
+         */
+        hideFullDescription() {
+            this.showFlag = false;
+            this.showIndex = null;
+            this.hasClosed = false;
+        },
+
+        /**
+         * ファイル説明を閉じる。
+         * 
+         */
+        closeFullDescription() {
+            this.hasClosed = true;
+        },
+
+        /** 当該ファイルが新しいファイルかチェック*/
+        isFileNew(updatedAt) {
+            const currentDate = Math.round(+new Date() / 1000);
+            const elapsedDay = (currentDate - updatedAt) / 86400;
+            const displayedDay = 14;
+            return displayedDay > elapsedDay;
+        },
+
+        onScroll(e) {
+            this.windowTop = window.top.scrollY
+        },
+    },
+    mounted() {
+        window.addEventListener("scroll", this.onScroll)
+    },
+    beforeDestroy() {
+        window.removeEventListener("scroll", this.onScroll)
     },
 };
 </script>
@@ -177,6 +251,38 @@ export default {
 
     .text-hidden {
         color: #cdcdcd;
+    }
+
+    .description {
+        position: relative;
+    }
+
+    .description-modal-wrapper {
+        width: 400px;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+
+    .description-modal {
+        position: fixed;
+        background-color: #fff;
+        border: 1px solid #ccc;
+        width: 400px;
+        padding: 10px;
+        white-space: pre-wrap;
+    }
+
+    .description-modal-text {
+        margin-top: 12px;
+    }
+
+    .new-indicator {
+        color: #fff;
+        background-color: #ff0000;
+        padding: 0 5px;
+        border-radius: 5px;
+        font-size: 12px;
     }
 }
 </style>
