ca3669f51a49a47a3ab9b01947cc37705179d2e6
d.naoto <naotolog@gmail.com>
2025-10-09 16:05:48 +0900

    fix: フォルダの右クリックメニューから「名前変更」を非表示にする (#585)
    - 資料一覧＞施設別の施設名（フォルダ）を右クリックした際に、「名前変更」メニューを表示しないように修正
    - contextMenuRules.jsのrenameRule()にフォルダチェックを追加
    - フォルダの誤操作による施設名変更を防止

--- src/components/blocks/mixins/contextMenuRules.js
+++ src/components/blocks/mixins/contextMenuRules.js
@@ -97,6 +97,10 @@ export default {
          * @returns {boolean}
          */
         renameRule() {
+            // フォルダ（施設名）の名前変更を禁止
+            if (this.firstItemType === 'dir') {
+                return false;
+            }
             return !this.multiSelect && this.$store.getters['fm/isEverySelectedItemRW'];
         },
 
